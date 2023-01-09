c00e8818948012a976d6c086badacade651792af
sjxiang <1535484943@qq.com>
2023-01-10 02:55:05 +0800

    接口重构

--- main.go
+++ main.go
@@ -1,27 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
 
-	"github.com/sjxiang/go-crawler/util"
+	"github.com/sjxiang/go-crawler/collect"
 )
 
 
 func main() {
-	url := "https://www.thepaper.cn/"
+	url := "https://book.douban.com/annual/2022?fullscreen=1&source=navigation"
 	
-	body, err := Fetch(url)
+	var f collect.Fetcher = collect.BaseFetch{}
+	body, err := f.Get(url)
 
 	if err != nil {
 		fmt.Printf("read content failed:%v", err)
@@ -29,13 +23,6 @@ func main() {
 	}
 
 
-	// 统计小 feature
-	numLinks := strings.Count(string(body), "<a")
-	fmt.Printf("当前页面链接总数为：%d 条\n", numLinks)
-
-	exist := strings.Contains(string(body), "疫情")
-	fmt.Printf("是否存在疫情相关：%v\n", exist)
-	
 	// 加载 HTML 文档
 	doc, err := goquery.NewDocumentFromReader(bytes.NewBuffer(body))
 	if err != nil {
@@ -51,38 +38,12 @@ func main() {
 }
 
 
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		util.Fatal(err)
-	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-	}
+func TestFeature(body string) {
+	// 统计小 feature
+	numLinks := strings.Count(body, "<a")
+	fmt.Printf("当前页面链接总数为：%d 条\n", numLinks)
 
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DeterminEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())  // 将 HTML 文本从特殊编码转换成 UTF-8 编码
-
-	return ioutil.ReadAll(utf8Reader)  
-}
-
-
-
-// 采样，确定编码属性
-func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
-	// 样本取 1024 字节
-	// HTML 文本如果少于 1024 字节，请求有问题
-	bytes, err := r.Peek(1024)
-
-	if err != nil {
-		fmt.Printf("fetch url error:%v", err)
-		return unicode.UTF8
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-
-	return e
+	exist := strings.Contains(body, "疫情")
+	fmt.Printf("是否存在疫情相关：%v\n", exist)	
 }
